e6977a219bcae8c5a6236bac8cc772e7c559de7c
KyleBB0102 <G00283711@stu.raritanval.edu>
2021-04-27 19:22:44 -0400

    Created a way to play game out of BlueJ

---
+++ GameMain.java
@@ -0,0 +1,19 @@
+
+/**
+ * This is where you can start my game
+ *
+ * @author Kyle Balao
+ * @version 04/27/21
+ */
+public class GameMain
+{
+    /**
+     * The starting point of the game
+     * @param args Program arguments
+     */
+    public static void main(String[] args)
+    {
+        Game game = new Game();
+        game.play();
+    }
+}
