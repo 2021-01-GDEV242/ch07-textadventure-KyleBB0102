b214fe6958f8ed91d24cf29fc6f0e5b0c8b79774
KyleBB0102 <G00283711@stu.raritanval.edu>
2021-04-27 20:07:43 -0400

    added some new rooms for game

--- Game.java
+++ Game.java
@@ -32,14 +32,18 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
-      
+        Room outside, theater, pub, lab, office, teleporter, cellar, victoryRoom;
+
         // create the rooms
         outside = new Room("outside the main entrance of the university");
         theater = new Room("in a lecture theater");
         pub = new Room("in the campus pub");
         lab = new Room("in a computing lab");
         office = new Room("in the computing admin office");
+        cellar = new Room(("in a dark cellar with two doors\nOne door to the east has a sign saying: Victory Room\n")+
+            ("and one to the west has a sign saying: AT YOUR OWN RISK\nthe door behind locked by itself so you are stuck"));
+        teleporter = new Room("in a room with random ammout of exits\nchoose one and see where u are");
+        victoryRoom = new Room("in the victory room!!!");
         
         // initialise room exits
         outside.setExit("east", theater);
